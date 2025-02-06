77503cec2983802b56c937c8eed125d9802ad6a4
amelia-liston <aliston@bu.edu>
2025-02-06 16:22:31 -0500

    Began implementing add button

--- projects/projects.js
+++ projects/projects.js
@@ -1,23 +1,42 @@
-function Add() {
+//Calculator Functions
 
-}
+//Add
+function addition() {
+    const num1Input = document.getElementById("num1").value;
+    const num1 = Number(num1Input);
 
-function Subtract() {
+    const num2Input = document.getElementById("num2").value;
+    const num2 = Number(num2Input);
 
-}
+    const result = num1 + num2;
 
-function Multiply(){
-
-}
-
-function Divide(){
-
-}
-
-function Power(){
-
-}
-
-function Clear(){
+    document.getElementById("output").innerHTML = String(result);
+    //document.getElementById("output").style.visibility = "visible";
     
+}
+
+//Subtract
+function subtraction() {
+
+}
+
+//Multiply
+function multiply(){
+
+}
+
+//Divide
+function divide(){
+
+}
+
+//Power
+function power(){
+
+}
+
+//Clear
+function clear(){
+    //document.getElementById("output").style.visibility = "hidden";
+    document.getElementById("output").innerHTML = "";
 }
